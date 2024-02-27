d8136a7734bedcdcebc652cee9db257797b3afd4
Subrath sapkota <subrathsapkota119@gmail.com>
2024-02-27 11:54:01 +0545

    Added sendMessage and getMessage controllers for handling message functionality

---
+++ backend/routes/message.routes.js
@@ -0,0 +1,9 @@
+import express from "express"
+import { getMessage, sendMessage } from "../controllers/message.controller.js"
+import { protectRoute } from "../middleware/protectRoute.js"
+
+export const messageRouter = express.Router()
+
+messageRouter.get('/:id',protectRoute , getMessage)
+messageRouter.post('/send/:id',protectRoute , sendMessage)
+
