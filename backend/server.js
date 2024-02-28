31998b7914969c5dfcac6384d904f8a6367a866a
Subrath sapkota <subrathsapkota119@gmail.com>
2024-02-28 11:09:21 +0545

    Implemented frontend UI with signup form for posting user data

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import { router } from "./routes/auth.routes.js";
 import { messageRouter } from "./routes/message.routes.js";
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors())
 
 app.use("/api/auth", router);
 app.use("/api/messages", messageRouter);
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg">
+        <h1 className="text-3xl font-semibold text-center">
+          Login <span className="text-blue-500">ChatApp</span>
+        </h1>
+        <form action="">
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your username"
+              className="input input-bordered w-full "
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          <Link
+            to="/signup"
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            {"Don't"} have an account?
+          </Link>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
